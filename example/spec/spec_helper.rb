a03e0d20e891731159d8ad6bd1f337a76b2a8fec
Wataru MIYAGUNI <gonngo@gmail.com>
2013-10-29 05:39:44 +0900

    Fix variable name for RSpec.configure block GH-29

--- example/spec/spec_helper.rb
+++ example/spec/spec_helper.rb
@@ -1,7 +1,7 @@
 require 'turnip_formatter'
 
 RSpec.configure do |config|
-  c.add_setting :project_name, :default => "My project"
+  config.add_setting :project_name, :default => "My project"
   config.add_formatter RSpecTurnipFormatter, 'report.html'
 end
 
