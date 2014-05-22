5944557bcca7a2712072c8f8c08ccc2fb7a86bda
Wataru MIYAGUNI <gonngo@gmail.com>
2014-05-22 23:49:50 +0900

    Create `ExampleHelper` that use instead of `shared_context_examples.rb`

---
+++ spec/support/example_helper.rb
@@ -0,0 +1,36 @@
+module ExampleHelper
+  def passed_example
+    base_example { expect(true).to be true }
+  end
+
+  def failed_example
+    example = base_example { expect(true).to be false }
+    example.exception.backtrace.push ':in step:0 `'
+    example
+  end
+
+  def pending_example
+    example = base_example { pending('Pending') }
+    example.execution_result[:pending_message] = 'No such step(0): '
+    example
+  end
+
+  private
+
+    def base_example(&assertion)
+      group = ::RSpec::Core::ExampleGroup.describe('Feature').describe('Scenario')
+      example = group.example('example', metadata, &assertion)
+      group.run(NoopObject.new)
+      example
+    end
+
+    def metadata
+      {
+        turnip_formatter: {
+          steps: [ { name: 'Step 1', extra_args: [], keyword: 'When' } ],
+          tags: []
+        },
+        file_path: '/path/to/hoge.feature'
+      }
+    end
+end
