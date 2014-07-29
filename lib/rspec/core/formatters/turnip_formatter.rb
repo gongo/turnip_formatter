fca9155252609c9ce9d45a0b22682eb7902b9757
Wataru MIYAGUNI <gonngo@gmail.com>
2014-07-29 18:50:21 +0900

    Separation code that depends on RSpec 2

--- lib/rspec/core/formatters/turnip_formatter.rb
+++ lib/rspec/core/formatters/turnip_formatter.rb
@@ -6,6 +6,7 @@ require 'turnip_formatter/scenario/failure'
 require 'turnip_formatter/scenario/pending'
 require 'turnip_formatter/printer/index'
 require 'turnip_formatter/printer/scenario'
+require_relative './turnip_formatter/for_rspec2'
 
 module RSpec
   module Core
@@ -15,6 +16,8 @@ module RSpec
 
         SCENARIO_TEMPORARY_OUTPUT_DIR = File.expand_path('./turnip_tmp')
 
+        include TurnipFormatter::ForRSpec2
+
         def initialize(output)
           super(output)
           @scenarios = []
@@ -23,36 +26,12 @@ module RSpec
           FileUtils.mkdir_p(SCENARIO_TEMPORARY_OUTPUT_DIR)
         end
 
-        def dump_summary(duration, example_count, failure_count, pending_count)
-          print_params = {
-            scenarios:      scenarios,
-            failed_count:   failure_count,
-            pending_count:  pending_count,
-            total_time:     duration,
-            scenario_files: scenario_output_files
-          }
-          output.puts ::TurnipFormatter::Printer::Index.print_out(print_params)
-          FileUtils.rm_rf(SCENARIO_TEMPORARY_OUTPUT_DIR)
-        end
-
-        def example_passed(example)
-          scenario = ::TurnipFormatter::Scenario::Pass.new(example)
-          output_scenario(scenario)
-        end
-
-        def example_pending(example)
-          clean_backtrace(example)
-          scenario = ::TurnipFormatter::Scenario::Pending.new(example)
-          output_scenario(scenario)
-        end
-
-        def example_failed(example)
-          clean_backtrace(example)
-          scenario = ::TurnipFormatter::Scenario::Failure.new(example)
-          output_scenario(scenario)
-        end
-
         private
+
+          def output_html(params)
+            output.puts ::TurnipFormatter::Printer::Index.print_out(params)
+            FileUtils.rm_rf(SCENARIO_TEMPORARY_OUTPUT_DIR)
+          end
 
           def output_scenario(scenario)
             filepath = SCENARIO_TEMPORARY_OUTPUT_DIR + "/#{scenario.id}.html"
@@ -63,12 +42,6 @@ module RSpec
 
             @scenario_output_files << filepath
             @scenarios << scenario
-          end
-
-          def clean_backtrace(example)
-            return if example.exception.nil?
-            formatted = format_backtrace(example.exception.backtrace, example)
-            example.exception.set_backtrace(formatted)
           end
       end
     end
