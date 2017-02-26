dfbf85ce699ba1f3d41fb37bd32b743308037cad
Wataru MIYAGUNI <gonngo@gmail.com>
2017-02-26 17:05:31 +0900

    Support multiple exception in steps

--- test/turnip_formatter/resource/scenario/test_failure.rb
+++ test/turnip_formatter/resource/scenario/test_failure.rb
@@ -6,12 +6,21 @@ module TurnipFormatter::Resource::Scenario
     include TurnipFormatter::TestHelper
 
     def test_status
-      @resource = Failure.new(scenario_example)
+      @resource = Failure.new(make_scenario(<<-EOS))
+        Scenario: This is a simple feature
+          Then [ERROR] it should die
+      EOS
+
       assert_equal(:failed, @resource.status)
     end
 
     def test_steps
-      @resource = Failure.new(scenario_example)
+      @resource = Failure.new(make_scenario(<<-EOS))
+        Scenario: This is a simple feature
+          When I attack it
+          Then [ERROR] it should die
+           And I get drop items
+      EOS
 
       expect = [:passed, :failed, :unexecute]
       actual = @resource.steps.map(&:status)
@@ -19,8 +28,30 @@ module TurnipFormatter::Resource::Scenario
       assert_equal(expect, actual)
     end
 
+    def test_steps_has_multiple_error
+      @resource = Failure.new(make_scenario(<<-EOS))
+        @aggregate_failures
+        Scenario: Error in after hook
+          When I attack it
+          Then [ERROR] it should die
+          When retry
+          Then [ERROR] it should die
+           And I get drop items
+      EOS
+
+      expect = [:passed, :failed, :passed, :failed, :passed]
+      actual = @resource.steps.map(&:status)
+
+      assert_equal(expect, actual)
+    end
+
     def test_steps_has_error_in_before_hook
-      @resource = Failure.new(scenario_error_before_hook)
+      @resource = Failure.new(make_scenario(<<-EOS))
+        @before_hook_error
+        Scenario: Error in before hook
+          When I attack it
+          Then it should die
+      EOS
 
       expect = [:failed, :unexecute, :unexecute]
       actual = @resource.steps.map(&:status)
@@ -30,7 +61,12 @@ module TurnipFormatter::Resource::Scenario
     end
 
     def test_steps_has_error_in_after_hook
-      @resource = Failure.new(scenario_error_after_hook)
+      @resource = Failure.new(make_scenario(<<-EOS))
+        @after_hook_error
+        Scenario: Error in after hook
+          When I attack it
+          Then it should die
+      EOS
 
       expect = [:passed, :passed, :failed]
       actual = @resource.steps.map(&:status)
@@ -40,65 +76,30 @@ module TurnipFormatter::Resource::Scenario
     end
 
     def test_steps_has_error_in_steps_and_after_hook
-      @resource = Failure.new(scenario_error_step_and_after_hook)
+      @resource = Failure.new(make_scenario(<<-EOS))
+        @after_hook_error
+        Scenario: Error in after hook
+          When I attack it
+          Then [ERROR] it should die
+           And I get drop items
+      EOS
 
-      expect = [:passed, :failed, :unexecute]
+      expect = [:passed, :failed, :unexecute, :failed]
       actual = @resource.steps.map(&:status)
 
       assert_equal(expect, actual)
-      assert_equal(TurnipFormatter::Resource::Step::Pass, @resource.steps.last.class)
+      assert_equal(TurnipFormatter::Resource::Step::AfterHook, @resource.steps.last.class)
     end
 
     private
 
-    def scenario_example
-      scenario_examples[0]
-    end
-
-    def scenario_error_before_hook
-      scenario_examples[1]
-    end
-
-    def scenario_error_after_hook
-      scenario_examples[2]
-    end
-
-    def scenario_error_step_and_after_hook
-      scenario_examples[3]
-    end
-
-    def scenario_examples
-      @@scenario_examples ||= (
-        feature = feature_build(feature_text)
-        run_feature(feature, '/path/to/test.feature')
-      )
-    end
-
-    def feature_text
-      <<-EOS
+    def make_scenario(scenario_text)
+      feature_text = <<-EOS
         Feature: A simple feature
-          Scenario: This is a simple feature
-            When I attack it
-            Then [ERROR] it should die
-             And I get drop items
-
-          @before_hook_error
-          Scenario: Error in before hook
-            When I attack it
-            Then it should die
-
-          @after_hook_error
-          Scenario: Error in after hook
-            When I attack it
-            Then it should die
-
-          @after_hook_error
-          Scenario: Error in after hook
-            When I attack it
-            Then [ERROR] it should die
-             And I get drop items
-
+          #{scenario_text}
       EOS
+      feature = feature_build(feature_text)
+      run_feature(feature, '/path/to/test.feature').first
     end
   end
 end
